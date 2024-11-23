a9fc8450c95a0ff53430a4b065019b1b9857b696
Furkan Recber <furkan.recber57@gmail.com>
2024-11-23 15:20:59 +0300

    commit

---
+++ packet_listener.py
@@ -0,0 +1,29 @@
+import scapy.all as scapy
+from scapy_http import http
+import optparse
+
+
+def get_user_input():
+    opts = optparse.OptionParser()
+
+    opts.add_option("-i", "--interface", dest="interface", help="Enter the interface to listen on")
+
+    inputs = opts.parse_args()[0]
+
+    if not inputs.interface:
+        print("Interface must be specified")
+    return inputs
+
+
+def listen_packet(interface):
+    scapy.sniff(iface=interface, store=false, prn=process_packet)
+
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
+
+
+user_input = get_user_input()
+listen_packet(user_input.interface)
